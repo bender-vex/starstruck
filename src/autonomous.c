aeaf54e06a6c0b74b2bf3649ba5c1f6e628af03c
Jacob Greenway <jagreenway12@gmail.com>
2016-07-09 14:48:25 -0500

    initial commit

---
+++ src/autonomous.c
@@ -0,0 +1,8 @@
+
+task main() {
+    // Calibrate sensors and robot
+
+    // Pass autonomous config into the function
+
+		// Run through the specified config
+}
---
+++ src/teleop.c
@@ -0,0 +1,17 @@
+
+#include "lib/joystick.h";
+
+task main() {
+
+    // Calibrate the sensors and robot
+
+    // Main control loop
+    while (true) {
+
+        // Poll joysticks and sensors
+
+        // Run raw data through PID controller, this ensures all input is sanitized
+
+        // Pass sanitized input to robot
+    }
+}
