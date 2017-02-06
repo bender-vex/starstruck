f7df8fba9c75a9f76d8d6f5ccfe16530fe85757d
Jacob Greenway <jagreenway12@gmail.com>
2017-02-06 17:31:44 -0600

    add constants for motor pins

--- include/drive.h
+++ include/drive.h
@@ -2,6 +2,14 @@
 #define MIN(num1, num2) (num1 < num2 ? num1 : num2)
 
 /**
+ * Define constants for motor ports
+ */
+#define MOTOR_FL 0
+#define MOTOR_FR 1
+#define MOTOR_BL 2
+#define MOTOR_BR 3
+
+/**
  * Use input from joystick to find what the motor outputs should be
  */
 void calculateArcadeDrive(const int movement, const int rotation, int *leftSpeed, int *rightSpeed);
