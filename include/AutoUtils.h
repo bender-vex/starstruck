ebc4ae2e7112cb9d71be45099d4ce7311dec24b6
Joseph P Foster <jpf150030@cslinux1.utdallas.edu>
2017-02-08 11:07:58 -0600

    Added PID handle, PID init, and PID free

--- include/AutoUtils.h
+++ include/AutoUtils.h
@@ -1,5 +1,22 @@
 #include <stdio.h>
 #include <math.h>
+#include <string.h>
+
+typedef struct
+{
+	float kp; // P constant
+	float ki; // I constant
+	float kd; // D constant
+	float target; //The value we want to reach
+	float last_error;
+	float error_cap;
+} PIDHandle;
+
+PIDHandle* initPID(float kp, float ki, float kd = 0.0f, float error_cap = 0.0f);
+void freePID(PIDHandle* handle);
+
 
 void placeHolder();
+
+
 
--- src/AutoUtils.c
+++ src/AutoUtils.c
@@ -1,5 +1,23 @@
 #include "AutoUtils.h"
 
+PIDHandle* initPID(float kp, float ki, float kd, float target, float error_cap)
+{
+	PIDHandle* handle = NULL;
+	handle = malloc(sizeof(PIDHandle));
+	memset(handle, 0, sizeof(PIDHandle));
+	handle->kp = kp;
+	handle->ki = ki;
+	handle->ki = ki;
+	handle->error_cap = error_cap;
+	handle->target = target;
+}
+
+void freePID(PIDHandle* handle)
+{
+	free(handle);
+}
+
+
 void placeHolder()
 {
 	printf("Place Holder\n");
