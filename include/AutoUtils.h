9b2b9761965197306ea897686c23b6217ef4db4b
Joseph P Foster <jpf150030@cslinux1.utdallas.edu>
2017-02-08 12:04:32 -0600

    Added set target function for PID

--- include/AutoUtils.h
+++ include/AutoUtils.h
@@ -10,9 +10,11 @@ typedef struct
 	float target; //The value we want to reach
 	float last_error;
 	float error_cap;
+	float integral;
 } PIDHandle;
 
 PIDHandle* initPID(float kp, float ki, float kd = 0.0f, float error_cap = 0.0f);
+void setPIDTarget(PIDHandle* handle, float target);
 void freePID(PIDHandle* handle);
 
 
--- src/AutoUtils.c
+++ src/AutoUtils.c
@@ -10,12 +10,20 @@ PIDHandle* initPID(float kp, float ki, float kd, float target, float error_cap)
 	handle->ki = ki;
 	handle->error_cap = error_cap;
 	handle->target = target;
+	handle->integral = 0;
 }
 
 void freePID(PIDHandle* handle)
 {
 	free(handle);
 }
+
+void setPIDTarget(PIDHandle* handle, float target)
+{
+	handle->integral = 0;
+	handle->target = target;
+}
+
 
 
 void placeHolder()
